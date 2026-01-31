e14944d0db56eb9a1d993e63c44c7bf3eb9725ac
BartSteer <bart.steer@hotmail.com>
2026-02-01 00:56:28 +0100

    scene manager + main menu

---
+++ masquerblade/scripts/start_screen.gd
@@ -0,0 +1,25 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	print("Start")
+	Global.game_controller.change_2d_scene("levelScreen")
+
+
+func _on_controls_pressed() -> void:
+	print("controls")
+	Global.game_controller.change_ui_scene("controlScreen")
+
+
+func _on_quit_pressed() -> void:
+	print("quit")
+	get_tree().quit()
