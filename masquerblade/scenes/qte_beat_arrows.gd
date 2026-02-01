eb479fedea1d0bd407c0070e062fb7f2ea5ebb88
rhiannonst <rhiannonthomson4@gmail.com>
2026-01-31 19:46:55 -0800

    add beat hit-zone
    when click spacebar keybinding, the console will log a correct safe-zone and incorrect unsafe click (missed a beat).

---
+++ masquerblade/scenes/qte_beat_arrows.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+#gets all related nodes, sprites, markers
+@onready var keyMatch: Sprite2D = $arrowKeyMatch
+@onready var pointer: Sprite2D = $pointer
+@onready var start: Marker2D = $start
+@onready var end: Marker2D = $end
+@onready var startSafe: Marker2D = $safeStartPos
+@onready var endSafe: Marker2D = $safeEndPos
+
+#gets the related enemy or player node too for dealing "mask off dmg" to their "hp"
+@onready var enemy: CharacterBody2D = $"../enemy"
+
+#direction of movement for falling arrows.
+var direction = 1 # down!
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float)->void:
+	if pointer.position.y <= start.position.y:
+		direction = 1
+	elif pointer.position.y >= end.position.y :
+		direction = -1
+	
+	pointer.position.y += direction 
+	
+	if Input.is_action_pressed("ui_accept"):
+		if pointer.position.y >= startSafe.position.y and pointer.position.y <= endSafe.position.y :
+			#enemy.hurt()
+			print("you've successfully clicked in the hit zone !")
+		else:
+			print("you've critically missed the beat")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
