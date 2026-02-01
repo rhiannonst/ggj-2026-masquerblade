4d89a0896bd0e7700c1673318870aa58c94a2b79
Sepehr Mehrabani <104735042+CookerBower@users.noreply.github.com>
2026-01-31 20:32:20 -0800

    Note spawner added

--- masquerblade/scripts/Rythm Game/note.gd
+++ masquerblade/scripts/Rythm Game/note.gd
@@ -1,6 +1,10 @@
 extends Area2D
-
-
+var speed = 100
+func initialize(lane):
+	print("note spawned")
+	position.x += lane*150
+	$AnimatedSprite2D.frame = lane
+	pass
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +12,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position.y -= speed * delta
+	
