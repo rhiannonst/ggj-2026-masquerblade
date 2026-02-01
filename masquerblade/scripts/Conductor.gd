de855d293f3295f2e1f7808ab671423c5fda1bdc
Sepehr Mehrabani <104735042+CookerBower@users.noreply.github.com>
2026-01-31 18:00:55 -0800

    Conductor Added

---
+++ masquerblade/scripts/Conductor.gd
@@ -0,0 +1,57 @@
+extends AudioStreamPlayer
+class_name Conductor
+
+@export var bpm := 100
+@export var measures := 4
+
+# Tracking the beat and song position
+var song_position = 0.0
+var song_position_in_beats = 1
+var sec_per_beat = 60.0 / bpm
+var last_reported_beat = 0
+var beats_before_start = 0
+var measure = 1
+
+# Determining how close to the beat an event is
+var closest = 0
+var time_off_beat = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sec_per_beat = 60.0/bpm
+
+func _physics_process(_delta):
+	if playing:
+		song_position = get_playback_position() + AudioServer.get_time_since_last_mix()
+		song_position -= AudioServer.get_output_latency()
+		song_position_in_beats = int(floor(song_position / sec_per_beat)) + beats_before_start
+		_report_beat()
+
+func _report_beat():
+	if last_reported_beat < song_position_in_beats:
+		if measure > measures:
+			measure = 1
+		Global.beat.emit(song_position_in_beats)
+		Global.measure.emit(measure)
+		last_reported_beat = song_position_in_beats
+		measure += 1
+
+# fake beats before song starts
+func play_with_beat_offset(num):
+	beats_before_start = num
+	$StartTimer.wait_time = sec_per_beat
+	$StartTimer.start()
+
+func _on_StartTimer_timeout():
+	song_position_in_beats += 1
+	if song_position_in_beats < beats_before_start - 1:
+		$StartTimer.start()
+	elif song_position_in_beats == beats_before_start - 1:
+		$StartTimer.wait_time = $StartTimer.wait_time - (AudioServer.get_time_to_next_mix() +
+														AudioServer.get_output_latency())
+		$StartTimer.start()
+	else:
+		play()
+		$StartTimer.stop()
+	_report_beat()
+	
